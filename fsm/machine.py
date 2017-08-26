6e4fe1d8611daf364cebff1029cf3af394ea48f8
Diego Billi <dbilli@debian8.7>
2017-08-26 11:13:11 +0200

    initial files

---
+++ fsm/machine.py
@@ -0,0 +1,369 @@
+
+
+GROUP_NAME = "g_%s"
+
+GROUP_NAME = "cluster_%s"
+
+#----------------------------------------------------------------------#
+#                                                                      #
+#----------------------------------------------------------------------#
+
+class MachineInput(object):
+	
+	__id = 0
+	
+	@staticmethod
+	def get_next_id():
+		v = MachineInput.__id
+		MachineInput.__id += 1
+		print __file__, v
+		return v
+	
+	
+	def __init__(self):
+		self.id = MachineInput.get_next_id()
+
+	def match(self, input):
+		raise Exception("NOT IMPLEMENTED")
+
+	def to_string(self):
+		return "<%s>" % (self.id)
+
+#----------------------------------------------------------------------#
+
+class Epsilon(MachineInput):
+	
+	e = u'\u03B5'
+	
+	def __init__(self):
+		super(Epsilon, self).__init__()
+
+	def to_string(self):
+		return "%s" % (Epsilon.e)
+
+
+
+class CharInput(MachineInput):
+	
+	def __init__(self, c):
+		super(CharInput, self).__init__()
+		self.c = c
+
+	def match(self, c):
+		return self.c == c
+		
+	def to_string(self):
+		return "{%s}" % (self.c)
+
+
+#----------------------------------------------------------------------#
+#                                                                      #
+#----------------------------------------------------------------------#
+
+class FiniteStateMachine(object): 
+
+	START   = 'START'
+	END     = 'END'
+
+	STATE_START  = 'start'
+	STATE_NORMAL = 'normal'
+	STATE_FINAL  = 'final'
+
+	EPSILON = Epsilon()
+
+	def __init__(self):
+		
+		self.states = {}
+
+		#
+		# transition_table = {
+		# 	<state> : {
+		# 		<input>: { 'next_state': <state> }
+		# 	
+		# 	}
+		# }
+		#
+
+		self.transition_table = {}
+		
+		self.inputs = {}
+		
+	def get_initial_state(self):
+		start = None
+		
+		for state, state_info in self.states.items():
+			 if (state_info['type'] == FiniteStateMachine.STATE_START):
+				start = state
+				break
+		
+		if not start:
+			raise Exceptio('NO START')
+			
+		return self.e_reduce(start)
+
+	def e_reduce(self, state):
+		
+			states = set()
+			
+			transitions = fsm.transition_table.get(state,{})
+			
+			for input_id, transitions_info in transitions.items():
+				
+				
+				input = fsm.inputs[input_id]
+
+				print input_id, input, transitions_info
+
+				for tran in transitions_info:
+					next_state = tran['next_state']
+					
+					print "\t", next_state
+
+					if input == FiniteStateMachine.EPSILON:
+						states.add(next_state)
+						states = states | self.e_reduce(next_state)
+					#else:
+					#	states.add(next_state)
+					
+			return states
+					
+					
+		
+	
+
+	def set_state(self, state, **options):
+		s = self.states.get(state, self.states.setdefault(state, {'name': state, 'type': FiniteStateMachine.STATE_NORMAL, 'group':None}) )
+		s.update( options )
+
+	def set_normal(self, state):
+		self.set_state(state, type=FiniteStateMachine.STATE_NORMAL)
+
+	def set_start(self, state):
+		self.set_state(state, type=FiniteStateMachine.STATE_START)
+
+	def set_end(self, state):
+		self.set_state(state, type=FiniteStateMachine.STATE_FINAL)
+
+	def set_e_transition(self, start_state, next_state):
+		
+		self.set_transition(start_state, FiniteStateMachine.EPSILON, next_state)
+
+	def set_transition(self, start_state, input, next_state):
+		
+		self.inputs[input.id] = input
+
+		self.set_state( start_state )
+		self.set_state( next_state  )
+
+		transition_action = {
+			'next_state': next_state
+		}
+
+		state_transitions = self.transition_table.get(start_state, self.transition_table.setdefault(start_state, {}))
+
+		transitions = state_transitions.get(input.id, state_transitions.setdefault(input.id, []))
+
+		transitions.append( transition_action )
+
+	def draw(self, file_name):
+
+		import pygraphviz as pgv
+
+		G = pgv.AGraph(
+			name='fsm', 
+			rankdir='LR', 
+			#rank='same',
+			directed=True, 
+			# size="24,1000",
+			#packmode='node',
+			#clusterrank='local', 
+			#compound=True, 
+			#rank='same ' + ' '.join(self.states.keys()),
+			#newrank=True,
+			#splines='line',
+			#ranksep='10'
+		)
+
+		for state, state_info in self.states.items():
+			options = {}
+			if state_info['type'] in [ FiniteStateMachine.STATE_START, FiniteStateMachine.STATE_FINAL]:
+				options['shape'] = "doublecircle"
+			G.add_node(state, **options)
+
+		"""
+		print G
+
+		states_by_group = {}
+		for s in self.states.values():
+
+			g_name = s['group']
+			if not g_name:
+				continue
+
+			g = states_by_group.get(g_name, states_by_group.setdefault(g_name,set()) )
+			g.add(s['name'])
+
+
+
+		print 1
+		
+		groups_by_parent = {}
+		for s in self.states.values():
+
+			group_name   = s['group']
+			parent_group = s['parent_group']
+			
+			print group_name, parent_group
+
+			groups_by_parent.get(group_name, groups_by_parent.setdefault(group_name, set()) )
+			
+			states = groups_by_parent.get(parent_group, groups_by_parent.setdefault(parent_group, set()) )
+			states.add(group_name)
+
+		print "----"
+
+		def _create_subgraphs(G, parent_graph=None, parent_group=None, level=0):
+
+			if parent_graph is None:
+				print ' ' * level, 'x'
+				parent_graph = G
+
+			group_names = groups_by_parent.get(parent_group, [])
+			for g_name in group_names:
+
+				states    = list(states_by_group[g_name])
+				subgroups = list([GROUP_NAME % (name) for name in groups_by_parent.get(g_name,[])])
+
+				graph = parent_graph.get_subgraph(name=GROUP_NAME % (g_name))
+				if graph is None:
+					graph = parent_graph.add_subgraph(
+						[], 
+						name=GROUP_NAME % (g_name), 
+						label=g_name, 
+						rankdir='LR', 
+						clusterrank='local', 
+						rank='same ' + ' '.join([ '%s;'%(s) for s in states+subgroups])
+					)
+					created = True
+				else:
+					created = False
+
+
+
+				print ' ' * level, g_name, states+subgroups, parent_group, repr(parent_graph), created
+
+				for state_name in states:
+					
+					state_info = self.states[state_name]
+					
+					options = {
+						'rank': 'same'
+					}
+					if state_info['type'] == FiniteStateMachine.STATE_FINAL:
+						options['shape']="doublecircle"
+					graph.add_node(state_name, **options)
+
+
+
+				_create_subgraphs(G, parent_graph=graph, parent_group=g_name, level=level+1)
+
+
+		_create_subgraphs(G)
+		
+		#print G
+		
+		#for g_name, states in states_by_group.items():
+		#	G.add_subgraph(states, name="cluster_%s" % (g_name), label=g_name, rankdir='LR')
+		"""
+	
+		for start_state, transitions in self.transition_table.items():
+
+			for input, state_transition in transitions.items():
+				
+				input = self.inputs[input]
+				
+				for transition_info in state_transition:
+
+					next_state = transition_info['next_state']
+
+					state_info = self.states[next_state]
+
+
+					options = {}
+					#if start_state > next_state:
+					#	options['weight'] = 0
+
+					G.add_edge(start_state, next_state, label=input.to_string(), **options)
+
+		print G
+
+		G.draw(file_name, prog='dot')
+
+		
+#----------------------------------------------------------------------#
+#                                                                      #
+#----------------------------------------------------------------------#
+
+class MachineRunner(object):
+	
+	def __init__(self):
+		self.current_state = set()
+		
+	def set_current_state(self, *state):
+		self.current_state = set(*state)
+	
+	def run_machine(self, fsm, input_data):
+		
+		if not self.current_state:
+			self.current_state = fsm.get_initial_state()
+		
+		print "INITIAL STATE", self.current_state
+		
+		final_states = set()
+		
+		for s in self.current_state:
+			
+			transitions = fsm.transition_table.get(s,[])
+			
+			for input_id, state_transitions in transitions.items():
+				
+				input = fsm.inputs[input_id]
+				
+				if input == FiniteStateMachine.EPSILON:
+					
+					continue
+				
+				if not input.match(input_data):
+					continue
+				
+				for tran in state_transitions:
+					next_state = tran['next_state']
+				
+					print "NEXT_STATE", next_state
+					
+					final_states |= fsm.e_reduce(next_state)
+
+		print final_states
+
+
+if __name__ == "__main__":
+	
+	fsm = FiniteStateMachine()
+
+	fsm.set_state(FiniteStateMachine.START, type=FiniteStateMachine.STATE_START)
+	fsm.set_state(FiniteStateMachine.END  , type=FiniteStateMachine.STATE_FINAL)
+	
+	fsm.set_state('a_s', group='A')
+	fsm.set_state('a_e', group='A')
+	
+	fsm.set_transition('a_s', CharInput('a'), 'a_e')
+
+	fsm.set_e_transition(FiniteStateMachine.START, 'a_s')
+
+	fsm.set_e_transition('a_e', FiniteStateMachine.END)
+
+	print fsm.states
+	
+	mr = MachineRunner()
+	
+	mr.run_machine(fsm, 'a')
---
+++ fsm/patterns.py
@@ -0,0 +1,262 @@
+
+from fsm.machine import FiniteStateMachine
+
+#----------------------------------------------------------------------#
+#                                                                      #
+#----------------------------------------------------------------------#
+
+class BasePatternMachine(object):
+	
+	def __init__(self, name):
+		self.name = name
+
+		self.s_start = '%s.s' % (name)
+		self.s_end   = '%s.e' % (name)
+
+	def create_state_machine(self, fsm=None, parent_group=None):
+		
+		if fsm is None:
+			fsm = FiniteStateMachine()
+
+		fsm.set_state(self.s_start, group=self.name, parent_group=parent_group)
+		fsm.set_state(self.s_end  , group=self.name, parent_group=parent_group)
+		
+		return fsm
+
+#----------------------------------------------------------------------#
+#                                                                      #
+#----------------------------------------------------------------------#
+
+class SinglePattern(BasePatternMachine):
+
+	def __init__(self, name, input):
+		
+		super(SinglePattern, self).__init__(name)
+
+		self.input = input
+
+		self.s_matched = '%s.m' % (name)
+
+
+	def create_state_machine(self, fsm=None, parent_group=None):
+
+		#         input        e
+		#    S -----------> M ---> E
+
+		fsm = super(SinglePattern, self).create_state_machine(fsm=fsm, parent_group=parent_group)
+
+		fsm.set_state(self.s_matched, group=self.name, parent_group=parent_group)
+
+
+		fsm.set_transition(self.s_start  , self.input                , self.s_matched)
+		
+		fsm.set_e_transition(self.s_matched, self.s_end)
+		
+
+		fsm.set_start(self.s_start)
+		fsm.set_end  (self.s_end)
+		
+		return fsm
+
+#----------------------------------------------------------------------#
+#                                                                      #
+#----------------------------------------------------------------------#
+
+class SequencePattern(BasePatternMachine):
+
+	def __init__(self, name, patterns):
+		super(SequencePattern, self).__init__(name)
+
+		self.patterns = patterns
+
+	def create_state_machine(self, fsm=None, parent_group=None):
+
+
+
+		#       e                                   e
+		#    S ---> machine1 > machine2 > machine3 ---> E
+
+		fsm = super(SequencePattern, self).create_state_machine(fsm=fsm, parent_group=parent_group)
+
+		fsm.set_start(self.s_start)
+		
+		for patterns in self.patterns:
+			patterns.create_state_machine(fsm=fsm, parent_group=self.name)
+
+		
+		prev_state = self.s_start
+
+		for pattern in self.patterns:
+
+			fsm.set_normal(pattern.s_start)
+			fsm.set_normal(pattern.s_end)
+
+			fsm.set_e_transition(prev_state, pattern.s_start)
+			
+			prev_state = pattern.s_end
+
+		fsm.set_e_transition(prev_state, self.s_end)
+
+		fsm.set_end  (self.s_end)
+		
+		return fsm
+
+#----------------------------------------------------------------------#
+#                                                                      #
+#----------------------------------------------------------------------#
+
+class AlternativePattern(BasePatternMachine):
+
+	def __init__(self, name, patterns):
+		super(AlternativePattern, self).__init__(name)
+
+		self.patterns = patterns
+
+	def create_state_machine(self, fsm=None, parent_group=None):
+
+		#       e             e
+		#    S ---> PATTERN 1 ---> E
+		#      ---> PATTERN 2 --->
+
+		fsm = super(AlternativePattern, self).create_state_machine(fsm=fsm, parent_group=parent_group)
+
+		fsm.set_start(self.s_start)
+		
+		for pattern in self.patterns:
+			pattern.create_state_machine(fsm=fsm, parent_group=self.name)
+
+		for pattern in self.patterns:
+
+			fsm.set_normal(pattern.s_start)
+			fsm.set_normal(pattern.s_end)
+			
+			
+			fsm.set_e_transition(self.s_start , pattern.s_start)
+			fsm.set_e_transition(pattern.s_end, self.s_end)
+
+		fsm.set_end  (self.s_end)
+		
+		return fsm
+
+#----------------------------------------------------------------------#
+#                                                                      #
+#----------------------------------------------------------------------#
+
+class RepetitionPattern(BasePatternMachine):
+
+	def __init__(self, name, pattern):
+		super(RepetitionPattern, self).__init__(name)
+
+		self.pattern = pattern
+
+	def create_state_machine(self, fsm=None, parent_group=None):
+		
+		fsm = super(RepetitionPattern, self).create_state_machine(fsm=fsm, parent_group=parent_group)
+		
+		fsm.set_start(self.s_start)
+		
+		self.pattern.create_state_machine(fsm=fsm, parent_group=self.name)
+
+
+		fsm.set_normal(self.pattern.s_start)
+		fsm.set_normal(self.pattern.s_end)
+
+		#              e
+		#          +-------+
+		#          |       |
+		#       e  V       |  e
+		#    S ---> PATTERN ----> E
+		
+
+		fsm.set_e_transition(self.s_start      , self.pattern.s_start)
+
+		fsm.set_e_transition(self.pattern.s_end, self.pattern.s_start)
+
+		fsm.set_e_transition(self.pattern.s_end, self.s_end)
+
+		fsm.set_end  (self.s_end)
+
+		return fsm
+
+#----------------------------------------------------------------------#
+#                                                                      #
+#----------------------------------------------------------------------#
+
+class OptionalPattern(BasePatternMachine):
+
+	def __init__(self, name, pattern):
+		super(OptionalPattern, self).__init__(name)
+
+		self.pattern = pattern
+
+	def create_state_machine(self, fsm=None, parent_group=None):
+
+		fsm = super(OptionalPattern, self).create_state_machine(fsm=fsm, parent_group=parent_group)
+
+		self.pattern.create_state_machine(fsm=fsm, parent_group=self.name)
+
+		fsm.set_start(self.s_start)
+
+		fsm.set_normal(self.pattern.s_start)
+		fsm.set_normal(self.pattern.s_end)
+
+		#              e
+		#    +--------------------+
+		#    |  e             e   v
+		#    S ---> PATTERN ----> E
+		
+		fsm.set_e_transition(self.s_start      , self.pattern.s_start)
+		fsm.set_e_transition(self.s_start      , self.s_end)
+		fsm.set_e_transition(self.pattern.s_end, self.s_end)
+
+		fsm.set_end  (self.s_end)
+
+		return fsm
+
+#----------------------------------------------------------------------#
+#                                                                      #
+#----------------------------------------------------------------------#
+
+if __name__ == "__main__":
+
+	from fsm.machine import CharInput
+
+	p = SinglePattern('A', CharInput('a'))
+	
+	p2 = SinglePattern('B', CharInput('b'))
+
+	p = AlternativePattern('OR', [p, p2] )
+	#p = SequencePattern('AB', [p, p2] )
+	
+	#p = RepetitionPattern('2', SinglePattern('1','a') )
+	
+	#p = OptionalPattern('1', 
+	#
+	#	#SinglePattern('5','a'), 
+	#	
+	#	#RepetitionPattern('2',
+	#	#	SequencePattern('4', [
+	#	#		SinglePattern('5','a'), 
+	#	#		SinglePattern('6','b'),
+	#	#	]),
+	#	#),
+	#
+	#	RepetitionPattern('2',
+	#		SequencePattern('3', [
+	#			SinglePattern('5','a'), 
+	#			SinglePattern('4','b'),
+	#		]),
+	#	),
+	#)
+
+	#p = OptionalPattern('?', p)
+	
+	#p = RepetitionPattern('+', p)
+
+	
+
+	
+	fsm = p.create_state_machine()
+	
+	fsm.draw('fsm.jpg')
+
